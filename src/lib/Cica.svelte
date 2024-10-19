64100913ae4ee641ec8a910af2596dfcba510ab9
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-19 12:10:25 +0200

    .

--- src/lib/Cica.svelte
+++ src/lib/Cica.svelte
@@ -6,9 +6,7 @@
 <button class="x" on:click={async () => {
     let answ = await fetch('http://localhost:8000/posts', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(adat)
     })
     if (answ.ok) {
