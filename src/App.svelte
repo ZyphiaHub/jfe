d756984fc6e1eb5d0043e7a4d421f3c9163bc408
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-19 11:34:50 +0200

    get data from server

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,19 @@
 <script>
   var cicanev = 'Mici'
   import Cica from './lib/Cica.svelte'
-
+  var data = []
 </script>
 
 <main>
   <Cica bind:name={cicanev}/>
   {cicanev}
+  <hr>
+  <button on:click={async () => {
+    data = await fetch('http://localhost:8000/posts')
+    .then(r => r.json())
+  }}>Cica</button>
+{#each data as d}
+  <p>{d.title}</p>
+{/each}
 </main>
 
